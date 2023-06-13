e960b4504394ae3584ea041b23dae66c55377930
Desquivel501 <3523114870101@ingenieria.usac.edu.gt>
2023-06-13 10:27:33 -0600

    Agregado formulario de inicio de sesión
    Agregado formulario de inicio de sesión

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,8 +1,10 @@
+import { Route } from 'wouter'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './Componentes/NavBar/Navbar'
+import Login from './pages/Login/Login'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +12,8 @@ function App() {
   return (
     <>
       <Navbar/>
-      
+      <Route path={"/Login"} component={Login}/>
+
     </>
   )
 }
