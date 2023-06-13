17ae0523831dd6b69dd5f53e6806150e41bb0137
Desquivel501 <3523114870101@ingenieria.usac.edu.gt>
2023-06-13 10:40:35 -0600

    Agregado registro de repartidor
    Agregado el formulario para registrar repartidores

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -7,6 +7,7 @@ import Navbar from './Componentes/NavBar/Navbar'
 import Login from './pages/Login/Login'
 import RegistroCliente from './pages/RegistroCliente/RegistroCliente'
 import RegistroEmpresa from './pages/RegistroEmpresa/RegistroEmpresa'
+import RegistroRepartidor from './pages/RegistroRepartidor/RegistroRepartidor'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,6 +18,7 @@ function App() {
       <Route path={"/Login"} component={Login}/>
       <Route path={"/RegistroCliente"} component={RegistroCliente}/>
       <Route path={"/RegistroEmpresa"} component={RegistroEmpresa}/>
+      <Route path={"/RegistroRepartidor"} component={RegistroRepartidor}/>
 
     </>
   )
