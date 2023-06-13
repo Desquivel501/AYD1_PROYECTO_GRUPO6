f5f50d0a1ba253d428224940cbacc5433393879d
DanielAcabal <danchiacabal@gmail.com>
2023-06-13 10:40:59 -0600

    Se agregó Landing Page

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,17 +1,14 @@
 import { Route } from 'wouter'
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './Componentes/NavBar/Navbar'
 import Login from './pages/Login/Login'
+import { Landing } from './pages/Landing/Landing'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Navbar/>
+      <Route path={"/"} component={Landing}/>
       <Route path={"/Login"} component={Login}/>
 
     </>
