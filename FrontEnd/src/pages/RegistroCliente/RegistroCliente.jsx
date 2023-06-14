7e74af5ce3e617a7a52b89f9b4071c5441bb36ea
Desquivel501 <3523114870101@ingenieria.usac.edu.gt>
2023-06-14 15:45:33 -0600

    Agregado registro de cliente
    Agregada la funcion para enviar los datos de registro de cliente al backend

--- FrontEnd/src/pages/RegistroCliente/RegistroCliente.jsx
+++ FrontEnd/src/pages/RegistroCliente/RegistroCliente.jsx
@@ -7,21 +7,38 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export default function RegistroCliente() {
+
   const handleSubmit = (event) => {
     event.preventDefault();
+
     const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
+    
+    // console.log({
+    //   email: data.get("email"),
+    //   password: data.get("password"),
+    // });
+
+    console.log(data)
+
+    var object = {};
+    data.forEach(function(value, key){
+      object[key] = value;
     });
-    fetch("http://localhost:3000/", {
+
+    fetch("http://127.0.0.1:3000/", {
       method: "POST",
-      mode: "no-cors",
-      body: data,
+      headers: {
+        'Content-Type':'application/json',
+        'Access-Control-Allow-Origin_Origin': '*'
+      },
+      body: JSON.stringify(object),
     });
-    event.target.reset()
+    
+    // event.target.reset()
   };
 
   const customTheme = createTheme({
