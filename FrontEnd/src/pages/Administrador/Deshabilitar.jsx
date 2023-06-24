a92af0ff1c72e24dbf7efc0e66a300a2634bd0ae
DanielAcabal <danchiacabal@gmail.com>
2023-06-24 15:32:28 -0600

    Se finalizó la funcionalidad de deshabilitar usuario

--- FrontEnd/src/pages/Administrador/Deshabilitar.jsx
+++ FrontEnd/src/pages/Administrador/Deshabilitar.jsx
@@ -1,9 +1,32 @@
-import { Box, Grid } from "@mui/material";
+import { Box, Button, Grid } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import usuarios from "../../mocks/usuarios.json";
+import users from "../../mocks/usuarios.json";
 import "./Deshabilitar.css";
+import "./Modal.css";
+import { useState } from "react";
+import { getData, postData } from "../../api/auth";
+import { useEffect } from "react";
 
 export function Deshabilitar() {
+  const [correo, setCorreo] = useState("");
+  const [usuarios, setUsuarios] = useState(users);
+  const [find, setFind] = useState("");
+
+  useEffect(() => {
+    const endpoint = "ObtenerUsuarios";
+    getData({ endpoint })
+      .then((data) => setUsuarios(data ?? users))
+      .catch((e) => console.log(e));
+  }, []);
+  const quitarCorreo = () => {
+    setCorreo("");
+  };
+  const filterUsers = () => {
+    if (find == "") return usuarios;
+    const regex = "^" + find;
+    const r = new RegExp(regex);
+    return usuarios.filter(({ id }) => r.test(id));
+  };
   const customTheme = createTheme({
     palette: {
       background: {
@@ -19,12 +42,13 @@ export function Deshabilitar() {
   };
   return (
     <ThemeProvider theme={customTheme}>
+      {correo != "" && <ModalDisable email={correo} close={quitarCorreo} />}
       <Box
         component="main"
         display="flex"
         height="80vh"
         sx={{ padding: "10vh" }}
-        width={"100%"}
+        width={"100vh"}
         margin="auto"
       >
         <Grid
@@ -40,6 +64,13 @@ export function Deshabilitar() {
           alignItems={"center"}
         >
           <h1>Deshabilitar Usuarios</h1>
+          <label>Buscar:</label>
+          <input
+            className="txt-find"
+            value={find}
+            onChange={(e) => setFind(e.target.value)}
+            placeholder="ejemplo@email.com"
+          />
           <p
             style={{
               display: "flex",
@@ -60,11 +91,16 @@ export function Deshabilitar() {
               className="lista-deshabilitar"
               style={{ listStyle: "none", padding: 0 }}
             >
-              {usuarios.map((value) => (
+              {filterUsers().map((value) => (
                 <li key={value.id}>
                   <span>{value.id}</span>
                   <span>{roles[value.rol]}</span>
-                  <button className="btn-disable">Deshabilitar</button>
+                  <button
+                    className="btn-disable"
+                    onClick={() => setCorreo(value.id)}
+                  >
+                    Deshabilitar
+                  </button>
                 </li>
               ))}
             </ul>
@@ -72,5 +108,55 @@ export function Deshabilitar() {
         </Grid>
       </Box>
     </ThemeProvider>
+  );
+}
+function Modal({ children }) {
+  return (
+    <div className="modal-overlay">
+      {children}
+    </div>
+  );
+}
+function ModalDisable({ email, close }) {
+  const [motivo, setMotivo] = useState("");
+  const handleClick = () => {
+    const endpoint = "deshabilitar";
+    const data = { correo: email, motivo };
+    postData({ endpoint, data })
+      .then((res) => console.log(res))
+      .catch((e) => console.log(e));
+  };
+  const handleChange = (e) => {
+    setMotivo(e.target.value);
+  };
+  return (
+    <Modal>
+      <div className="modal-disable">
+        <h1>¿Por qué deshabilitará este usuario ?</h1>
+        <h2>{email}</h2>
+        <textarea
+          onChange={handleChange}
+          cols={50}
+          rows={5}
+          placeholder="Motivo..."
+          value={motivo}
+          style={{ resize: "none" }}
+        />
+        <Button
+          onClick={handleClick}
+          type="button"
+          sx={{ mt: 1, mb: 2, bgcolor: "#F2890D", color: "black" }}
+        >
+          Deshabilitar
+        </Button>
+        <Button
+          onClick={close}
+          type="button"
+          sx={{ mb: 2, bgcolor: "#F2890D", color: "black" }}
+        >
+          Cerrar
+        </Button>
+      </div>
+    </Modal>
   );
 }
