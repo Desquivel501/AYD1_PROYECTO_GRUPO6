1a86649c58ce3fb68c0e4cde0c827709f82ddef1
DanielAcabal <danchiacabal@gmail.com>
2023-06-28 15:44:36 -0600

    Se agregó funcionalidad de nuevaDirección y comisiones

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1034,8 +1034,8 @@ app.post('/topProductoEmpresa', cors(), (req, res)=>{
     res.status(200).json(results);
   });
 });
-
+/*
 // Inicia el servidor
 app.listen(3000, () => {
   console.log('Servidor escuchando en el puerto 3000');
-});
+});*/
